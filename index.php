702e3d792e2b2861dccce2e6cd07abd690ada0b1
PedroBorundaMixen <pedro.borunda@mixen.mx>
2017-02-02 16:35:37 -0700

    Mailer

--- index.php
+++ index.php
@@ -37,10 +37,7 @@
                 EXPLORE AMAZING BRANDS
             </h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-                natoque penatibus et magnis dis parturient montes, nascetur
-                ridiculus mus. 
+                 
             </p>
             <div class="col-sm-4">
                 <div class="img-container">
@@ -88,20 +85,21 @@
                 <p class="phone">
                     +52(614)427.6941
                 </p>
-                <a class="mail" href="mailto:contacto@mascolorselp.com">
-                    contacto@mascolorselp.com
+                <a class="mail" href="mailto:contacto@mascolorstx.com">
+                    contacto@mascolorstx.com
                 </a>
                 <p class="address">
                     11601 Pelícano Suite E1<br>
                     El Paso, Tx.
                 </p>
                 <div class="form-container">
-                    <form action="mailer.php" method="POST" id="contact-form">
+                    <form action="mailer.php" method="POST" id="form">
                         <input type="text" name="name" id="name" placeholder="Name">
                         <input type="number" name="phone" id="phone" placeholder="Phone">
                         <input type="email" name="email" id="email" placeholder="Email">
                         <textarea name="mssg" id="mssg" cols="30" rows="3" placeholder="Message"></textarea>
                         <div class="text-right">
+                            <input type="hidden" name="bttnContacto">
                             <input type="submit" value="" id="form-submit">
                             <div id="contact-form-output"></div>
                         </div>
@@ -114,7 +112,7 @@
         </div>
         <div class="bg right-side">
             <button id="bttnMapa">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.0881523182707!2d-106.07250458443701!3d28.627119982419845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86ea5cac021712e5%3A0x8941f49f258d5502!2sMas+Colors!5e0!3m2!1ses!2smx!4v1485468014119" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3392.9362624941227!2d-106.30645912287058!3d31.744940672954534!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86e7437e05df51db%3A0xa2d97812fe42b657!2s11601+Pellicano+Dr%2C+El+Paso%2C+TX+79936%2C+EE.+UU.!5e0!3m2!1ses-419!2smx!4v1486076831369" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
             </button>
         </div>
     </div>
@@ -131,10 +129,10 @@
                 </a>
             </div>
             <div class="img-container right-side">
-                <a href="mailto:contacto@mascolorselp.com">
+                <a href="mailto:contacto@mascolorstx.com">
                     <img src="img/icons/mail-blue.png" alt="">
                 </a>
-                <a target="_blank" href="https://www.facebook.com/">
+                <a target="_blank" href="https://www.facebook.com/MasColors-1571150519866617/">
                     <img src="img/icons/fb-blue.png" alt="">
                 </a>
             </div>
--- js/app.js
+++ js/app.js
@@ -9,25 +9,61 @@ $(window).scroll(function () {
     }
 });
 
-$('.navbar-nav li').click(function() {
-    $('.navbar-nav li').removeClass('active');
-    $(this).addClass('active');
-});
-
-$('li.dropdown a').on('click', function (event) {
-    $('li.dropdown.dropdown-submenu').removeClass('open');
-    $(this).parent().toggleClass('open');
-});
-
-$('body').on('click', function (e) {
-    if (!$('li.dropdown').is(e.target) 
-        && $('li.dropdown').has(e.target).length === 0 
-        && $('.open').has(e.target).length === 0)
-    {
-        $('li.dropdown').removeClass('open');
-    }
-});
-
 $("#bttnMapa").click(function() {
     $(this).find("iframe").css('pointer-events', 'all');
+});
+
+//Ajax contact form
+$(function() {
+    var close = '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>';
+    // Get the form.
+    var form = $('#form');
+    // Get the messages div.
+    var formMessages = $('#form-output');
+
+    // Set up an event listener for the contact form.
+    $(form).submit(function(e) {
+        // Stop the browser from submitting the form.
+        e.preventDefault();
+        $("#form-submit").val("Enviando...");
+
+        // Serialize the form data.
+        var formData = $(form).serialize();
+
+        // Submit the form using AJAX.
+        $.ajax({
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: formData
+        })
+            .done(function(response) {
+            // Make sure that the formMessages div has the 'success' class.
+            $(formMessages).removeClass('alert alert-danger');
+            $(formMessages).addClass('alert alert-success');
+
+            // Set the message text.
+            $(formMessages).html(close+response);
+
+            // Clear the form.
+            $('#name').val('');
+            $('#phone').val('');
+            $('#email').val('');
+            $('#mssg').val('');
+            $("#form-submit").val("Enviar");
+        })
+            .fail(function(data) {
+            // Make sure that the formMessages div has the 'error' class.
+            $(formMessages).removeClass('alert alert-success');
+            $(formMessages).addClass('alert alert-danger');
+
+            // Set the message text.
+            if (data.responseText !== '') {
+                $(formMessages).html(close+data.responseText);
+                $("#form-submit").val("Enviar");
+            } else {
+                $(formMessages).html(close+'Oops! Ocurrió un error, intentelo nuevamente.');
+                $("#form-submit").val("Enviar");
+            }
+        });
+    });
 });
